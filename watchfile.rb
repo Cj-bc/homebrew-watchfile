a3bfd8fe79de2b2457678bac74cc2d8ba90d2fa5
Cj-bc <cj-bc@y-modify.org>
2018-05-19 09:18:58 +0900

    Ver: 0.1.0
    First release.
    Contains:
            basic functions
            bpkg support
            homebrew support

---
+++ watchfile.rb
@@ -0,0 +1,9 @@
+class Watchfile < Formula
+  homepage "https://github.com/Cj-bc/watchfile"
+  url "https://github.com/Cj-bc/watchfile/archive/0.1.0.tar.gz"
+
+
+  def install
+    system "mv", "watchfile.sh", "watchfile"
+    bin.install "watchfile"
+  end
