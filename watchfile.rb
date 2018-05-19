093629fa760bc1b14ebaafe2b1837c58f7614d6c
Cj-bc <cj-bc@y-modify.org>
2018-05-19 09:31:45 +0900

    add checksum

--- watchfile.rb
+++ watchfile.rb
@@ -1,6 +1,7 @@
 class Watchfile < Formula
   homepage "https://github.com/Cj-bc/watchfile"
   url "https://github.com/Cj-bc/watchfile/archive/0.1.0.tar.gz"
+  sha256 "2ec308f87e50f57c7ff40ba70e3d54dcff46e5e9954694ece60ba29f04a4e857"
 
 
   def install
