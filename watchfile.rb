78f98050dafbf89de82794caf0e23344d2f12258
Cj-bc <cj-bc@y-modify.org>
2018-05-20 09:35:36 +0900

    Ver: v0.2.0
    contains:
            * color support for todo.txt

--- watchfile.rb
+++ watchfile.rb
@@ -1,9 +1,9 @@
 class Watchfile < Formula
   desc "watch & display file, even changing will be displayed!"
   homepage "https://github.com/Cj-bc/watchfile"
-  url "https://github.com/Cj-bc/watchfile/archive/v0.1.0.tar.gz"
+  url "https://github.com/Cj-bc/watchfile/archive/v0.2.0.tar.gz"
   head "https://github.com/Cj-bc/watchfile.git"
-  sha256 "2ec308f87e50f57c7ff40ba70e3d54dcff46e5e9954694ece60ba29f04a4e857"
+  sha256 "43476ea0791b25d0e836d03dbbc361f1cd058fcde6d7e0afdef8bee5ff895562"
 
 
   def install
